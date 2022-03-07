d4ca27a4126b84503d98d716457c4496800bd258
Samuel Adekunle <samuelbeebest@gmail.com>
2022-03-08 00:19:58 +0100

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
-import 'package:avocado_lemon_cake/auth/login.dart';
-import 'package:avocado_lemon_cake/auth/register.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
-import 'Screens/home_page.dart';
 import 'function/provider/theme_provider.dart';
 import 'routes/app_routes.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,7 +26,8 @@ class MyApp extends StatelessWidget {
               themeMode: themeprovider.themeMode,
               theme: MyThemes.lightTheme,
               darkTheme: MyThemes.darkTheme,
-              home: const HomePage(),
+              routes: AppRoutes().routes,
+              initialRoute: '/homepage',
             );
           },
         );
--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../auth/forgot_pass.dart';
 import '../auth/login.dart';
 import '../auth/register.dart';
 import '../screens/home_page.dart';
@@ -9,5 +10,6 @@ class AppRoutes {
     '/login': (context) => const LoginScreen(),
     '/register': (context) => const RegisterScreen(),
     '/homepage': (context) => const HomePage(),
+    '/forget-pass': (context) => const ForgetPassword(),
   };
 }
